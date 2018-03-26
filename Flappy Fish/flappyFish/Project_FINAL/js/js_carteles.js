e4d8bdb3e85cac9fc8e4f4bbe9f3e65026529a21
Sebastian Geroli <sebastiangeroli@outlook.com>
2018-03-26 16:19:55 +0200

    Actualizacion
    Ultimas versiones agregadas

---
+++ Flappy Fish/flappyFish/Project_FINAL/js/js_carteles.js
@@ -0,0 +1,21 @@
+ /*js_carteles.js*/
+ 
+ 
+var modal1 = document.getElementById('id01');
+var modal2 = document.getElementById('id02');
+
+window.onclick = function(event) {
+    if (event.target === modal1) {
+        modal1.style.display = "none";
+		alert("asdfghj");
+    }
+}
+ 
+ window.onclick = function(event) {
+    if (event.target === modal2) {
+        modal2.style.display = "none";
+    }
+}
+ 
+ 
+ 
